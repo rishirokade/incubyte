948189d7277cb3bd6f72c6aaee04e9db0cf00567
rishirokade <rishikeshrokade7@gmail.com>
2025-03-13 21:56:41 +0530

    return sum from function

--- Add.js
+++ Add.js
@@ -1,6 +1,15 @@
 function add(s) {
     // checking empty string condition
     if (!s) return 0;
+
+    // split string by , and convert into number
+    const numberArray = s.split(",").map((value) => {
+        const number = Number(value);
+        return isNaN(number) ? 0 : number;
+    });
+
+    // create sum using reduced
+    return numberArray.reduce((acc, curr) => acc + curr, 0);
 }
 
 add("1,5");
